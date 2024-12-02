5cfd64c9bdbf2ed3900b8c82043586a827f6478d
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-12-02 16:45:12 -0400

    ok

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const alunos = [
+    {
+        _id:0,
+        nome:"chico melato",
+        notas:{
+            portuques:[],
+            matematica:[],
+            historia:[],
+            ciencia:[],
+        },
+        media:{
+            port
+        }
+
+         
+    },
+    {
+        _id:1,
+        nome:"willy thiago",
+        notas:{
+            portuques:[],
+            matematica:[],
+            historia:[],
+            ciencia:[],
+        },
+        media:{
+            port
+        }
+
+         
+    }
+]
